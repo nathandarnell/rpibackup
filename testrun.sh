8ebe7abd5d7c32945e6383d62811a266cbc4c2bd
nathandarnell <nathanrdarnell@gmail.com>
2014-05-05 19:24:29 -0700

    Update testrun.sh
    Major moves towards all functions

--- testrun.sh
+++ testrun.sh
@@ -5,9 +5,10 @@
 ## Also includes some code from various other sources
 ## Changed by Nathan Darnell
 ## Adapted by me to work on my RPi systems and be more flexible
-## Comments welcome as I am more of a Google-r than a programmer
-## TODO: save to a logfile, change to all functions, 
 
+##################################################################
+## CONFIGURE
+##################################################################
 SERVICES="avahi-daemon deluge-daemon cron bubbleupnpserver fail2ban minidlna nginx nullmailer monitorix"    ## Declare what services to stop and start
 SUBDIR=RaspberryPi2_backups   ## Setting up backup directories
 DIR=/media/1TB/$SUBDIR  ## Change to where you want the backups to be stored
@@ -15,30 +16,64 @@ KEEPDAILY=7       ## How many daily (7 = 7 daily backups kept at one time), week
 KEEPWEEKLY=28     ## As of now, this needs to be in days (4 weeks = 28 days = 4 backups kept for the weekly backup)
 KEEPMONTHLY=90    ## So does this (3 months = 90 days = 3 monthly backups kept)
 TESTRUN=0         ## Set this to "0" if you want to write to the disk.  CHange it to do a test run to just use "TOUCH" and clean up after itself.
+##################################################################
+## /CONFIGURE
+##################################################################
 
-## Setting up echo fonts
-red='\e[0;31m'
-green='\e[0;32m'
-cyan='\e[0;36m'
-yellow='\e[1;33m'
-purple='\e[0;35m'
-NC='\e[0m' #No Color
-bold=$(tput bold)
-normal=$(tput sgr0)
+OFILE="$DIR/backup_$(date +%Y%m%d_%H%M%S)"      # Create a filename with datestamp for our current backup (without .img suffix)
+OFILEFINAL=$OFILE.daily.img   # Create final filename, with suffix
+OFILEFINALWEEKLY=${OFILEFINAL/daily./weekly.}   # Create final weekly filename, with suffix
+OFILEFINALMONTHLY=${OFILEFINAL/daily./monthly.}  # Create final monthly filename, with suffix
 
+function InitialSetup {
+      ## Screen clear
+      clear
+
+      ## Start the backup
+      echo -e "Starting RaspberryPi backup process!"
+      echo ""
+
+      ## First check if pv package is installed, if not, install it first
+      PACKAGESTATUS=$(dpkg -s pv | grep Status);
+
+      if [[ $PACKAGESTATUS == S* ]]
+      then
+            echo -e "Package 'pv' is installed"
+            echo ""
+      else
+            echo -e "Package 'pv' is NOT installed"
+            echo -e "Installing package 'pv' + 'pv dialog'. Please wait..."
+            echo ""
+            sudo apt-get -y install pv
+      fi
+
+      ## Check if backup directory exists
+      if [ ! -d "$DIR" ];   
+      then
+            echo -e "Backup directory $DIR doesn't exist, creating it now!"
+            sudo mkdir $DIR
+      fi
+}
+
+
+
+##################################################################
 ##List all the files with ".img" in $DIR
 ##Then list all files without ".img"
 ##################################################################
 function ListBackups {
       echo ""
-      echo -e "${purple}Last backups on HDD:${NC}"
-      sudo find $DIR -maxdepth 1 -name "*.img" -exec ls {} \;
+      echo -e "Last backups on HDD:"
+      sudo find $DIR -maxdepth 1 -name "*.img"
       echo ""
-      echo -e "${purple}Failed backups on HDD:${NC}"
+      echo -e "Failed backups on HDD:"
       sudo find $DIR/ -maxdepth 1 -mindepth 1 ! -name "*.img"
       echo ""
 }
 
+
+
+##################################################################
 ## Borrowed and adapted from http://hustoknow.blogspot.com/2011/01/bash-script-to-check-disk-space.html
 ##################################################################
 function CheckDiskSpace {
@@ -50,7 +85,7 @@ function CheckDiskSpace {
       SOURCEDISKSPACE=$(df -H / | sed '1d' | awk '{print $2}' | cut -d'G' -f1)
 
       # Disk capacity check
-      echo -e "${green}${bold}Checking if there is enough diskspace for one more backup...${NC}${normal}"      
+      echo -e "Checking if there is enough diskspace for one more backup..."      
       if [ ${SOURCEDISKSPACE} -ge ${DESTDISKSPACE} ]; then
             echo "Not enough disk space on source ($DESTDISKSPACE) for backup, need $SOURCEDISKSPACE"
             exit 1
@@ -60,154 +95,106 @@ function CheckDiskSpace {
 }
 
 
-## Does a test run of the write with TOUCH and cleans up after itself
+
 ##################################################################
-function TestRun {
-      echo ""
-      echo -e "${green}${bold}Doing a test run of backing up SD card to .IMG file on HDD${NC}${normal}"
-      touch $DIR/$OFILE 
-}
-
-
-## Screen clear
-clear
-
-## Start the backup
-echo -e "${green}${bold}Starting RaspberryPi backup process!${NC}${normal}"
-echo ""
-
-## First check if pv package is installed, if not, install it first
-PACKAGESTATUS=$(dpkg -s pv | grep Status);
-
-if [[ $PACKAGESTATUS == S* ]]
-   then
-      echo -e "${cyan}${bold}Package 'pv' is installed${NC}${normal}"
-      echo ""
-   else
-      echo -e "${yellow}${bold}Package 'pv' is NOT installed${NC}${normal}"
-      echo -e "${yellow}${bold}Installing package 'pv' + 'pv dialog'. Please wait...${NC}${normal}"
-      echo ""
-      sudo apt-get -y install pv
-fi
-
-CheckDiskSpace
-
-# Check if backup directory exists
-if [ ! -d "$DIR" ];   
-   then
-      echo -e "${yellow}${bold}Backup directory $DIR doesn't exist, creating it now!${NC}${normal}"
-      sudo mkdir $DIR
-fi
-
-# Create a filename with datestamp for our current backup (without .img suffix)
-OFILE="$DIR/backup_$(date +%Y%m%d_%H%M%S)"
-
-# Create final filename, with suffix
-OFILEFINAL=$OFILE.daily.img
-
-# Create final weekly filename, with suffix
-OFILEFINALWEEKLY=${OFILEFINAL/daily./weekly.}
-
-# Create final monthly filename, with suffix
-OFILEFINALWEEKLY=${OFILEFINAL/daily./monthly.}
-
-# First sync disks
-sync; sync
-
-## Move this to the top
+## Turn on and off the services listed in $SERVICES
 ##################################################################
 function DeclaredServices {
       
-      
+      case "$1" in
+      stop)
+      ## Quit the declared services
+      for service in $SERVICES
+      do
+            echo "Stopping $service..."
+            sudo /etc/init.d/$service stop
+                  if (ps ax | grep -v grep | grep $service > /dev/null)
+                  then
+                        echo "$service not stopped!"
+                        break
+                  fi   
+      done 
+      ;;
+
+      start)
+      ##Restart the stopped services
+            for service in $SERVICES
+            do
+                  echo "Starting $service..."
+                  sudo /etc/init.d/$service start
+            done 
+      ;;
+      esac
 }
 
-## Quit the declared services
-for service in $SERVICES
-do
-echo "Stopping $service..."
-sudo /etc/init.d/$service stop
-if (ps ax | grep -v grep | grep $service > /dev/null)
-then
-echo "$service not stopped!"
-break
-fi
-done 
 
-## Check the TESTRUN variable and write to the disk or don't. Each returns a "0" for success and "1" for failure
-if [ $TESTRUN == 0 ] 
-then
-      WriteBackupToDisk
-else
-      TestRun
-fi
+
+##################################################################
 ## Begin the backup process, should take about 20 minutes from a 16GB Class 10 SD card to HDD and double that over Samba
 ##################################################################
 function WriteBackupToDisk {
+      # First sync disks
+      sync; sync
       echo ""
-      echo -e "${green}${bold}Backing up SD card to .IMG file on HDD${NC}${normal}"
+      echo -e "Backing up SD card to .IMG file on HDD"
       SDSIZE=$(sudo blockdev --getsize64 /dev/mmcblk0);
       sudo pv -tpreb /dev/mmcblk0 -s $SDSIZE | dd of=$OFILE bs=1M conv=sync,noerror iflag=fullblock
-}
-# Wait for DD to finish and catch result
-RESULT=$?
-
-##Restart the stopped services
-for service in $SERVICES
-do
-echo "Starting $service..."
-sudo /etc/init.d/$service start
-done 
-
-# If command has completed successfully do weekly and monthly backups, if not, delete created files
-if [ $RESULT = 0 ];
-   then
-     sudo mv $OFILE $OFILEFINAL
+      ## Finalize the backup
+      sudo mv $OFILE $OFILEFINAL
       echo ""
-      echo -e "${green}${bold}RaspberryPI backup process completed! The Backup file is: $OFILEFINAL${NC}${normal}"
-      echo -e "${yellow}Looking for backups older than $KEEPDAILY days${NC}"
+      echo -e "RaspberryPI backup process completed! The Backup file is: $OFILEFINAL"
+      echo -e "Looking for backups older than $KEEPDAILY days"
 ## TODO: make this IF statement actually go after files older than 7 days as well as more than 7 in number
       if [ "$(find $DIR -maxdepth 1 -name "*.daily.img" | wc -l)" -ge "$KEEPDAILY" ]; then
-            echo -e "${yellow}Removing backups older than $KEEPDAILY days${NC}"
+            echo -e "Removing backups older than $KEEPDAILY days"
             sudo find $DIR -maxdepth 1 -name "*.daily.img" -exec rm {} \;
             ListBackups
       else
-            echo -e "${yellow}There were no backups older than $KEEPDAILY days to delete${NC}"
+            echo -e "There were no backups older than $KEEPDAILY days to delete"
       fi
       sudo find $DIR -maxdepth 1 -name "*.daily.img" -mtime +$KEEPDAILY -exec ls {} \; ## Is there a problem with using "ls" here?
 
-      echo -e "${cyan}If any backups older than $KEEPDAILY days were found, they were deleted${NC}"
+      echo -e "If any backups older than $KEEPDAILY days were found, they were deleted"
       
+}
+
+
+
+##################################################################
+## Make weekly and monthly backups
+##################################################################
+function WeeklyMonthlyBackups {
       ## Make weekly backup
-      echo -e "${yellow}Checking for weekly backups${NC}"
+      echo -e "Checking for weekly backups"
       if [[ ! -f $DIR/*.weekly.img ]]; then 
-            echo -e "${yellow}No weekly backups found so I am making the first one...${NC}"
+            echo -e "No weekly backups found so I am making the first one..."
             CheckDiskSpace
             sudo pv $OFILEFINAL > $OFILEFINALWEEKLY
       else
-            echo -e "${yellow}Weekly backups were found. Checking if a new one is needed...${NC}"
+            echo -e "Weekly backups were found. Checking if a new one is needed..."
             if test $(find $DIR/*.weekly.img -mtime -7)
             then
-                  echo -e "${yellow}None are older than 7 days${NC}" 
+                  echo -e "None are older than 7 days" 
             else
-                  echo -e "${yellow}Need a new weekly backup.  Making it now...${NC}"
+                  echo -e "Need a new weekly backup.  Making it now..."
                   CheckDiskSpace
                   sudo pv $OFILEFINAL > $OFILEFINALWEEKLY	## pv gives the user some feedback
                   sudo find $DIR -maxdepth 1 -name "*weekly.img" -mtime +$KEEPWEEKLY -exec rm {} \;	## Remove any weekly backups that are too old
             fi
       fi
       ## Make monthly backup
-      echo -e "${yellow}Checking for monthly backups${NC}"
+      echo -e "Checking for monthly backups"
       if [[ ! -f $DIR/*.monthly.img ]]; then 
-            echo -e "${yellow}No monthly backups found so I am making the first one...${NC}"
+            echo -e "No monthly backups found so I am making the first one..."
             CheckDiskSpace
             sudo pv $OFILEFINAL > $OFILEFINALMONTHLY
       else
-            echo -e "${yellow}Monthly backups were found. Checking if a new one is needed...${NC}"
+            echo -e "Monthly backups were found. Checking if a new one is needed..."
             if test $(find $DIR/*.monthly.img -mtime -30)
             then
-                  echo -e "${yellow}None are older than 30 days${NC}" 
+                  echo -e "None are older than 30 days" 
             else
-                  echo -e "${yellow}Need a new monthly backup.  Making it now...${NC}"
+                  echo -e "Need a new monthly backup.  Making it now..."
                   CheckDiskSpace
                   sudo pv $OFILEFINAL > $OFILEFINALMONTHLY
                   sudo find $DIR -maxdepth 1 -name "*monthly.img" -mtime +$KEEPMONTHLY -exec rm {} \;
@@ -218,9 +205,65 @@ if [ $RESULT = 0 ];
 # Else remove attempted backup file
    else
       echo ""
-      echo -e "${red}${bold}Backup failed!${NC}${normal}"
+      echo -e "Backup failed!"
       sudo rm -f $OFILE
       ListBackups
-      echo -e "${red}${bold}RaspberryPI backup process failed!${NC}${normal}"
+      echo -e "RaspberryPI backup process failed!"
       exit 1
 fi
+}
+
+
+
+##################################################################
+## Does a test run of the write with TOUCH and cleans up after itself
+##################################################################
+function TestRun {
+      echo ""
+      echo -e "Doing a test run of backing up SD card to .IMG file on HDD"
+      touch $DIR/$OFILE 
+
+if [ $RESULT = 0 ];
+   then
+     sudo pv $OFILE > $OFILEFINAL
+      echo ""
+      echo -e "RaspberryPI backup process completed! The Backup file is: $OFILEFINAL"
+      echo -e "Looking for backups older than $KEEPDAILY days"
+## TODO: make this IF statement actually go after files older than 7 days as well as more than 7 in number
+      if [ "$(find $DIR -maxdepth 1 -name "*.daily.img" | wc -l)" -ge "$KEEPDAILY" ]; then
+            echo -e "Removing backups older than $KEEPDAILY days"
+            sudo find $DIR -maxdepth 1 -name "*.daily.img" -exec rm {} \;
+            ListBackups
+      else
+            echo -e "There were no backups older than $KEEPDAILY days to delete"
+      fi
+      sudo find $DIR -maxdepth 1 -name "*.daily.img" -mtime +$KEEPDAILY -exec ls {} \; ## Is there a problem with using "ls" here?
+
+      echo -e "If any backups older than $KEEPDAILY days were found, they were deleted"
+     
+      WeeklyMonthlyBackups
+      
+      ListBackups
+      
+      echo "Cleaning up after myself by deleting the files that were just made"
+      
+      sudo rm -f $OFILE $OFILEFINAL $OFILEFINALWEEKLY $OFILEFINALMONTHLY
+}
+
+
+
+
+
+DeclaredServices stop
+
+## Check the TESTRUN variable and write to the disk or don't. Each returns a "0" for success and "1" for failure
+if [ $TESTRUN == 0 ] 
+then
+      WriteBackupToDisk
+else
+      TestRun
+fi
+
+DeclaredServices start
+
+
