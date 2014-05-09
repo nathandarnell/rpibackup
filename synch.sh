57757a9f25c09acec8407cef55a1210eb08c72f2
nathandarnell <nathanrdarnell@gmail.com>
2014-05-09 16:30:34 -0700

    Update synch.sh
    Uses functions and a case statement to get parameters from CLI and save me ~6+ keystrokes.

--- synch.sh
+++ synch.sh
@@ -1,40 +1,47 @@
 #!bin/bash
 ## Saves me a few keystrokes
-if [ $# -lt 2 ]
-then
-        echo "Usage : $0 [COMMAND] [BRANCH]"
-        echo "Commands:"
-        echo "s - synch with GitHub and change file permissions"
-        echo "r - run rpibackup.sh"
-        echo "a - synch with GitHub, change file permissions, and run rpibackup.sh"
-        exit
-fi
-      
-case "$1" in
-      s)
+function Synch {
             echo "Synching with GitHub..."
             echo ""
             git reset --hard
             git pull origin "$2"
             chmod +x rpibackup.sh synch.sh
+  }
+
+function Run {
+            echo "Running the backup..."
+            echo ""
+            ./rpibackup.sh
+            find "$DIR" -maxdepth 1 -name '*weekly.img' | sort
+  }
+
+function Error {
+                echo "Usage : $0 [COMMAND] [BRANCH]"
+                echo "Commands:"
+                echo "s - synch with GitHub and change file permissions"
+                echo "r - run rpibackup.sh"
+                echo "a - synch with GitHub, change file permissions, and run rpibackup.sh"
+                exit
+  }
+
+        if [ ! $# -eq 2 ] && [ ! "$1" == r ]
+        then
+          Error
+        fi
+
+case "$1" in
+      s)
+        Synch "$@"
       ;;
       r)
-            ./rpibackup.sh
-            echo "The Weekly backups are:"
-            echo ""
-            find "$DIR" -maxdepth 1 -name '*weekly.img' | sort
+        Run
       ;;
       a)
             s
             r
       ;;
       *)
-        echo "Usage : $0 [COMMAND] [BRANCH]"
-        echo "Commands:"
-        echo "s - synch with GitHub and change file permissions"
-        echo "r - run rpibackup.sh"
-        echo "a - synch with GitHub, change file permissions, and run rpibackup.sh"
-        exit
+        Error
       ;;
 esac
 
