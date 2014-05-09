0a349bbaef95dc1f8109eae8e9d9fd4817c3fb19
nathandarnell <nathanrdarnell@gmail.com>
2014-05-09 16:15:14 -0700

    Update rpibackup.sh
    I gotta stop using the testrun.sh script and use these dang branches!

--- rpibackup.sh
+++ rpibackup.sh
@@ -26,14 +26,84 @@ KEEPWEEKLY=28                 ## As of now, this needs to be in days (4 weeks = 
 KEEPMONTHLY=90                ## So does this (3 months = 90 days = 3 monthly backups kept)
 TESTRUN=0                     ## Set this to "0" if you want to write to the disk.  Change it to do a test run to just use "TOUCH" and clean up after itself.
 TESTRUNPERM=1                 ## Set this to "0" if you don't want to leave the TestRun files on the disk, but delete them (Leaving the files can be useful for testing the weekly and monthly backups)
+INCREMENTALBACKUPS=0          ## Set this to 0 if you want to disable incremental backups or make it 1 to enable them
 ##################################################################
 ## /CONFIGURE
 ##################################################################
 
+##################################################################
+## SET VARIABLES
+##################################################################
 OFILE="$DIR/backup_$(date +%Y%m%d_%H%M%S)"      # Create a filename with datestamp for our current backup (without .img suffix)
 OFILEFINAL=$OFILE.daily.img   # Create final filename, with suffix
 OFILEFINALWEEKLY=${OFILEFINAL/daily./weekly.}   # Create final weekly filename, with suffix
 OFILEFINALMONTHLY=${OFILEFINAL/daily./monthly.}  # Create final monthly filename, with suffix
+##################################################################
+## /SET VARIABLES
+##################################################################
+
+function MakeIncrementalBackup {
+    if [ ! $INCREMENTALBACKUPS == 0 ]
+    then
+        ## Check if there is a weekly backup to use as the base for the delta file
+        if [ -s "$(find $DIR -maxdepth 1 -name '*weekly.img' | sort -rn | head -1)" ]
+        then
+            ## Base the delta on the most resent weekly backup
+            DELTAORIG="$(find $DIR -maxdepth 1 -name '*weekly.img' | sort -rn | head -1)"
+        else
+            echo "There are no weekly backups to base the delta on.  Something must have gone wrong..."
+            return
+        fi
+        ## Make a delta of the daily backup using the weekly backup (??) as the original
+        xdelta3 -e -s $DELTAORIG $OFILEFINAL $OFILEFINAL.patch
+        
+        ## Now that the delta has been made, delete the fullsize daily backup
+        rm -f $OFILEFINAL
+    else
+        return
+    fi
+
+}
+
+function RestoreIncrementalBackup {
+            
+            
+            function RestoreMenu {
+    echo "1. Number1"
+    echo "2. Number2"
+    echo "3. Number3"
+    echo "4. All"
+    echo "5. Quit"
+}
+
+while true ; do
+    RestoreMenu
+    read choices
+    for choice in $choices ; do
+	case "$choice" in
+	    1)
+		echo "Number One" ;;
+	    2)
+		echo "Number Two" ;;
+	    3)
+		echo "Number Three" ;;
+	    4)
+		echo "Numbers One, two, three" ;;
+	    5)
+		echo "Exit"
+		exit 0 ;;
+	    *)
+		echo "Please enter number ONLY ranging from 1-5!"
+		;;
+	esac
+    done
+done
+            
+            
+            
+    xdelta3 -d -v -s backup_20140508_152105.daily.img patch0837 remadebackup_20140508_222319.daily.img
+
+}
 
 function InitialSetup {
       echo ""
