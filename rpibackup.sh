de0a1d3f00475246aa69e45400263b85beaa19f6
nathandarnell <nathanrdarnell@gmail.com>
2014-05-12 08:51:42 -0700

    Update rpibackup.sh
    Moved when services come on again and added a minutes and hours counter to the end.

--- rpibackup.sh
+++ rpibackup.sh
@@ -552,14 +552,19 @@ DeclaredServices stop
 if [[ $TESTRUN == 0 ]] 
 then
   WriteBackupToDisk
+  DeclaredServices start
   WeeklyMonthlyBackups
   MakeIncrementalBackup
 else
   TestRun
+  DeclaredServices start
 fi
 
-DeclaredServices start
 
-##Figure out how many seconds the backup took...
+
+##Figure out how many seconds/minutes/hours the backup took...
 ENDTIME=$(date +%s)
-echo "It took $(($ENDTIME - $STARTTIME)) seconds to complete this backup!"
+ELAPSEDTIME=$((ENDTIME - STARTTIME))
+echo "It took $ELAPSEDTIME seconds to complete this backup!"
+echo "That's $((ELAPSEDTIME / 60)) minutes!"
+echo "And $((ELAPSEDTIME /60 /60)) hours!"
