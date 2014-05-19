1348a3406f4d60a82db756c44667fd4ee2b1d87a
nathandarnell <nathanrdarnell@gmail.com>
2014-05-19 09:28:27 -0700

    Test.

--- rpibackup.sh
+++ rpibackup.sh
@@ -111,6 +111,8 @@ if [[ ! $INCREMENTALBACKUPS == 0 ]]; then
     ## Base the delta on the most resent weekly backup
     DELTAORIG="$(find "$DIR" -maxdepth 1 -name "*weekly.img" | sort -rn | head -1)"
   else
+  find "$DIR" -maxdepth 1 -name "*weekly.img" ## Test
+  find "$DIR" -maxdepth 1 -name "*weekly.img" | sort -rn | head -1 ## Test
     echo "There are no weekly backups to base the delta on.  Something must have gone wrong..."
     return 1 ## I think this means there was an error??
   fi
